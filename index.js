da5ca7dead81b4ea4d1abe01dca431fbe4827c9a
Nariman Emamian <nariman.emamian@sweco.se>
2019-01-14 17:06:54 +0100

    yearly update

--- index.js
+++ index.js
@@ -3,9 +3,49 @@ var admin = require('firebase-admin')
 var https = require('https')
 var nodemailer = require('nodemailer')
 var config = require('./config/config.json')
-var serviceAccount = require('./config/credentials.json')
+var serviceAccount = require('./config/serviceAccount.json')
 var smtpConfig = require('./config/smtpConfig.json')
 
+var actualIp, storedIp
+
+var options = {
+  host: 'api.ipify.org',
+  port: 443,
+  path: '/?format=json',
+  method: 'GET'
+}
+
+let data = ''
+var req = https.request(options, (res) => {
+  res.on('data', (chunk) => {
+    data += chunk
+  })
+  res.on('end', () => {toDb()})
+})
+
+req.end()
+
+// Write to Firebase if there is any change
+function toDb () {
+  actualIp = JSON.parse(data).ip
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: config.databaseURL
+  })
+  admin.database().ref('/').once('value').then((data) => {
+    storedIp = data.val().ip
+
+    if (actualIp !== storedIp) {
+      sendEmail(actualIp)
+        .then(() => admin.database().ref('/').update({ip: actualIp}))
+        .then(() => process.exit())
+    } else {
+      process.exit()
+    }
+  })
+}
+
+// Notify user via email
 function sendEmail (ip) {
 
   let transporter = nodemailer.createTransport(smtpConfig)
@@ -19,44 +59,3 @@ function sendEmail (ip) {
 
   return transporter.sendMail(mailOptions)
 }
-
-var actualIp, storedIp
-
-var options = {
-  host: 'api.ipify.org',
-  port: 443,
-  path: '/?format=json',
-  method: 'GET'
-}
-
-var req = https.request(options, (res) => {
-  let data = ''
-  res.on('data', (chunk) => {
-    data += chunk
-  })
-  res.on('end', () => {
-    actualIp = JSON.parse(data).ip
-  })
-})
-// write data to request body
-/*req.write('data\n')
-req.write('data\n')*/
-req.end()
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: config.databaseURL
-})
-
-
-admin.database().ref('/').once('value').then((data) => {
-  storedIp = data.val().ip
-
-  if (actualIp !== storedIp) {
-    sendEmail(actualIp)
-      .then(() => admin.database().ref('/').update({ip: actualIp}))
-      .then(() => process.exit())
-  } else {
-    process.exit()
-  }
-})
